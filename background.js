9fb19d1e001065e10615f77fbafa457af66883cd
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-06-27 21:34:21 -0400

    Merged in the redirect branch and expanded on it. Blocking and timer now works, but the hourglass does not yet reset.

--- background.js
+++ background.js
@@ -1,32 +1,93 @@
-function reload(message) {
-    if (message.reload) {
-        setSiteFilter();
+// background.js
+let timeAllowance = 1 * 60;
+
+var hourglass = null;
+var timeRemaining = timeAllowance;
+
+function gatekeeper(event) {
+    browser.storage.sync.get("hourglass").then(result => {
+        if (result != 0) {      // Permitted Stage
+            isTresspassing().then(tresspass => {
+                if (tresspass) {
+                    // Start or continue timer
+                    if (!hourglass) hourglass = setInterval(sandTick, 1000);
+                } else {
+                    // Stop timer and update hourglass
+                    clearInterval(hourglass);
+                    hourglass = null;
+                    browser.storage.sync.set({hourglass: timeRemaining})
+                        .catch(error => console.log(`Error: ${error}`));
+                }
+            });
+        } else {                // Blocking Stage
+            isTresspassing().then(tresspass => {
+                if (tresspass) {
+                    // Redirect tab to blocking page
+                    blockAll();
+                }
+            });
+        }
+    });
+}
+
+function isTresspassing() {
+    return getTresspassing(result => {
+        // If result is not empty, then is tresspassing.
+        if (result) return true;
+        else return false;
+    });
+}
+
+function sandTick() {
+    // Decrement timeRemaining.
+    timeRemaining -= 1;
+    // Update the status panel.
+    browser.runtime.sendMessage({"timeRemaining": timeRemaining});
+    // Check if time is up, in which case redirect all tresspassers.
+    if (timeRemaining <= 0) {
+        browser.storage.sync.set({hourglass: 0})
+            .catch(error => console.log(`Error: ${error}`));
+        clearInterval(hourglass);
+        timeRemaining = 0;
+        blockAll();
     }
 }
 
-function gatekeep(details) {
-    console.log("blocking this page");
-    console.log(browser.extension.getURL("block_page.html"));
-    return {redirectUrl: browser.extension.getURL("block_page.html")};
+function blockAll() {
+    getTresspassing(result => {
+        for (let tab of result) {
+            browser.tabs.update(
+                tab.id,
+                {url: browser.extension.getURL("block_page.html")});
+        }
+    }).catch(error => console.log(`Error: #{error}`));
+
 }
 
-function setSiteFilter() {
-    browser.webRequest.onBeforeSendHeaders.removeListener(gatekeep);
-    browser.storage.sync.get("blacklist")
+function getTresspassing(process) {
+    return browser.storage.sync.get("blacklist")
         .then(result => {
             var urls = [];
             for (let site of result.blacklist) {
                 urls.push("*://*." + site + "/*");
                 urls.push("*://" + site + "/*");
             }
-            browser.webRequest.onBeforeSendHeaders.addListener(gatekeep,
-                                                               {urls: urls},
-                                                               ["blocking"]);
+            return browser.tabs.query({url: urls, active: true});
         })
-        .catch(error => {
-            console.log(`Error: ${error}`);
-        });
+        .then(process).catch(error => console.log(`Error: ${error}`));
 }
 
-browser.runtime.onMessage.addListener(reload);
-setSiteFilter();
+// Create a timer if it doesn't exist. Once timer is established, add the gatekeeper.
+browser.storage.sync.get("hourglass").then(result => {
+    if (Object.keys(result.hourglass).length == 0) {
+        console.log("Creating new hourglass");
+        browser.storage.sync.set({hourglass: timeAllowance})
+            .catch(error => console.log(`Error: ${error}`));
+        timeRemaining = timeAllowance;
+    }
+    else timeRemaining = result.hourglass;
+}).then(result => {
+console.log(timeRemaining);
+    browser.tabs.onUpdated.addListener(gatekeeper);
+    browser.tabs.onActivated.addListener(gatekeeper);
+});
--- popup/status_panel.js
+++ popup/status_panel.js
@@ -1,3 +1,20 @@
+function updateClock(secondsRemaining) {
+    var time = "";
+console.log(secondsRemaining);
+    var hours = Math.floor(secondsRemaining / 3600);
+    hours = (hours < 10) ? "0" + hours : hours;
+    secondsRemaining %= 3600;
+
+    var minutes = Math.floor(secondsRemaining / 60);
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    secondsRemaining %= 60;
+
+    var seconds = (secondsRemaining < 10) ? "0" + secondsRemaining : secondsRemaining;
+console.log(hours+":"+minutes+":"+seconds);
+    document.querySelector("#timer").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
 document.querySelector('#settings').addEventListener('click', function (event) {
     browser.runtime.openOptionsPage().catch(error => console.log(`Error: ${error}`));
 });
+browser.runtime.onMessage.addListener(message => updateClock(message.timeRemaining));
