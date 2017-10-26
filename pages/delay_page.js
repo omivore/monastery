7dbe04ecb38bea5e155ae01dddaa05e6810bd6b9
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-10-25 23:33:51 -0400

    Timer on the delay page now counts down delay

--- pages/delay_page.js
+++ pages/delay_page.js
@@ -1,8 +1,13 @@
 // delay_page.js
 
 var delay = null;
+var delayCounter = null;
+
+var initialized = false;
 var delayLength = null;
 var destination = null;
+
+let delayLeft = document.querySelector('#delayLeft');
 
 function release() {
     // Once delay is done, add this tab to the passedDelay list
@@ -14,19 +19,42 @@ function release() {
 }
 
 function restartDelay() {
+    if (!initialized) {
+        console.log("Deferring until initialization");
+        return;
+    }
+
     console.log("Beginning delay timer");
-    delay = setTimeout(release, delayLength * 1000);
+    if (delay == null) {
+        delay = setTimeout(release, delayLength * 1000);
+    }
+
+    delayLeft.textContent = delayLength;
+    if (delayCounter == null) {
+        delayCounter = setInterval(() => {
+            delayLeft.textContent = delayLeft.textContent - 1;
+        }, 1000);
+    }
 }
 
 function initialize(delayTime, destinationURL) {
-    if (delay == null) restartDelay();
+    console.log("Initializing delays");
     if (delayLength == null) delayLength = delayTime;
     if (destination == null) destination = destinationURL;
+    if (delay == null) restartDelay();
+    initialized = true;
 }
 
 document.onfocus = restartDelay;
 
 document.onblur = () => {
     console.log("Resetting delay");
+
     clearTimeout(delay);
+    clearInterval(delayCounter);
+
+    delay = null;
+    delayCounter = null;
+
+    delayLeft.textContent = delayLength;
 };
