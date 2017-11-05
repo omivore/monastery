113a2ddaabb98b35e67d15e915aafb51d5bb2376
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-11-04 22:17:05 -0400

    Add approving edit exception for timeouts of 0
    Fixes #10: you can change the timeout, but it doesn't take effect until
    the next reset i.e. the next day. So that's a nice compromise.

--- options/options.js
+++ options/options.js
@@ -2,8 +2,13 @@
 
 /// Disable select options if time is up for today
 function approveEdit() {
-    return browser.storage.sync.get('hourglass')
+    return browser.storage.sync.get(['hourglass', 'timeout'])
         .then(result => {
+            // Exception: If timeout is 0, then let them change whatever
+            if (result.timeout == 0) {
+                return true;
+            }
+
             if (result.hourglass == 0) {
                 document.querySelector('#disabledNotice').classList.add('shown');
                 // Add hidden back in after css animation plays
